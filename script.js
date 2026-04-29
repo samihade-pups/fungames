fa0c57449832e0412e44497a0e7116c9920e4622
fish <voronoffya@gmail.com>
2026-04-29 18:35:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var n = 0
+
+const modal_container = document.getElementById('modal_container');
+
+
+function modal() {
+    modal_container.classList.add('show')
+};
+
+function zakrit() {
+    modal_container.classList.remove('show')
+}
+
+function not() {
+    event.currentTarget.classList.add('red')
+
+}
+
+function yes() {
+    event.currentTarget.classList.add('green')
+    n = n + 1
+}
+
+
+
+function showScore() {
+    let score = n
+    window.alert(score);
+  }
